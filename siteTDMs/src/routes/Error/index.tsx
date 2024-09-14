ca1cadd2201ef8feee94d712fe132fc56e37dafb
vn740 <vininery2006@gmail.com>
2024-09-14 15:23:19 -0300

    Iniciando Criação de Rotas

---
+++ siteTDMs/src/routes/Error/index.tsx
@@ -0,0 +1,9 @@
+import { BrowserRouter, Routes } from "react-router-dom";
+
+export default function Home(){
+    return(
+        <BrowserRouter>
+            <Routes></Routes>
+        </BrowserRouter>
+    )
+}
